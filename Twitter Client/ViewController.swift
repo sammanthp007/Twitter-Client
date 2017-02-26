d391891a125d18a37e7b3b7d1466b567d9f263d4
sammanthp007 <samman.thapa@bison.howard.edu>
2017-02-26 17:19:57 -0500

    Initial Commit

---
+++ Twitter Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter Client
+//
+//  Created by samman on 2/26/17.
+//  Copyright © 2017 samman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

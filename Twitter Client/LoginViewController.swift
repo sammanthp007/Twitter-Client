d4bd06201fe8888e7800b6308fb4e48e304b52e8
sammanthp007 <samman.thapa@bison.howard.edu>
2017-02-28 19:09:24 -0500

    request for request token

--- Twitter Client/LoginViewController.swift
+++ Twitter Client/LoginViewController.swift
@@ -27,6 +27,16 @@ class LoginViewController: UIViewController {
         // session manager: for making Get or Post requests using sessions
         let twitterClient = BDBOAuth1SessionManager(baseURL: NSURL(string: "https://api.twitter.com") as URL!, consumerKey: "Egs4PG34sQWvqD2zCLMjrHdOI", consumerSecret: "90GSSJxs9j6NJzUXbWJ7rkhu7jVXCTHJKfVcosDYlPVZLEIT9i")
         
+        // fetch request token using a generic OAUTH one process for twitter to verify that the actual api holder is making this call
+        // the path to the request token can be found in app.twitter.com page
+        // in even of a sucess, request for my request token
+        twitterClient?.fetchRequestToken(withPath: "oauth/request_token", method: "GET", callbackURL: nil, scope: nil, success: {
+            (requestToken: BDBOAuth1Credential?) -> Void in
+            print ("Received request token")
+        }, failure: {
+            (Error) in
+            print ("Error \(Error?.localizedDescription)")
+        })
         
     }
     /*

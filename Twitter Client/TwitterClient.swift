f094e5a138a88f38220586747bf43606888a3a6e
sammanthp007 <samman.thapa@bison.howard.edu>
2017-03-01 00:09:37 -0500

    create an api client (handler) for twitter

---
+++ Twitter Client/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  Twitter Client
+//
+//  Created by samman on 3/1/17.
+//  Copyright © 2017 samman. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1SessionManager {
+
+}
